3cc91cd87f8004b5dd3a9a9d08ac145a89a0b22c
JeremyVunCode4Fun <94739483+JeremyVun@users.noreply.github.com>
2021-11-20 16:53:03 +0000

    Initial commit

---
+++ hints.md
@@ -0,0 +1,3 @@
+# Hints
+## Hint 1
+A dictionary is just a mapping between two different objects. If you have the correct mappings, you can crawl through a dictionary to find a path...
---
+++ main.py
@@ -0,0 +1,35 @@
+"""
+Dictionary Challenge 2 - Destination City
+
+Description:
+You are given the array paths, where paths[i] = [cityAi, cityBi] means there exists a direct path going from cityAi to cityBi. Return the destination city, that is, the city without any path outgoing to another city.
+
+It is guaranteed that the graph of paths forms a line without any loop, therefore, there will be exactly one destination city.
+
+Example:
+Input:
+  paths = [
+    ["London","New York"],
+    ["New York","Lima"],
+    ["Lima","Sao Paulo"]
+  ]
+  start = "London"
+Correct Output:
+  "Sao Paulo"
+"""
+
+
+########
+# Write your code in this function
+########
+def solution(paths, start):
+  pass
+
+
+########
+# This code wil run your solution against a bunch of test cases
+########
+from tests.runner import run
+
+if __name__ == "__main__":
+  run(solution)
